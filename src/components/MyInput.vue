654bb90b807ad1197b20e3a0b59784dd0526c397
Máté Donka <mate.salamon@gmail.com>
2018-12-11 21:45:31 +0100

    my input component

---
+++ src/components/MyInput.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+.field
+  label.label {{ label }}
+  .control(:class="{'has-icons-left': icon}")
+    input.input(:type="type", :placeholder="placeholder" :value="value" @input="$emit('input', $event.target.value)")
+    span.icon.is-small.is-left(v-if="icon")
+      i.fas(:class="`fa-${icon}`")
+</template>
+
+<script>
+export default {
+  props: {
+    label: { type: String },
+    type: { type: String, default: 'text' },
+    value: { type: String },
+    placeholder: { type: String },
+    icon: { type: String }
+  }
+}
+</script>
---
+++ src/globalComponents.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import MyInput from '@/components/MyInput.vue'
+
+Vue.component('my-input', MyInput)
