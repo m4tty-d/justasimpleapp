9ce6951a3c01d22b6edbec690c9c4b7c91c72b2c
Máté Donka <mate.salamon@gmail.com>
2018-12-12 11:10:51 +0100

    auth interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,32 @@
 import axios from 'axios'
+import store from '@/store'
 const apiUrl = process.env.VUE_APP_API_URL
 
-export default axios.create({
+const apiInstance = axios.create({
   baseURL: apiUrl
 })
+
+apiInstance.interceptors.request.use(
+  function(config) {
+    const token = store.state.token
+
+    if (token != null) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+  },
+  function(err) {
+    return Promise.reject(err)
+  }
+)
+
+apiInstance.interceptors.response.use(null, function(err) {
+  if (err.status === 401) {
+    store.commit('logout')
+  }
+
+  return Promise.reject(err)
+})
+
+export default apiInstance
